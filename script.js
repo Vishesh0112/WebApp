634693bef661a6a0fe0c6612a10c3e862d002107
Vishesh0112 <visheshbansal369@gmail.com>
2021-02-05 10:35:55 -0500

    Fixing bugs

--- script.js
+++ script.js
@@ -4,12 +4,25 @@ var list = document.getElementById('todo-list');
 var inputBox = document.getElementById('todo-input');
 var currentInputValue = "";
 
+function doSomething() {
+    return false;
+}
+
 inputBox.addEventListener('input',function(e){
     currentInputValue = e.target.value;
+    console.log("inside inputbox event listener");
+    //  if(e.keyCode===13){
+    //      console.log("inside enter key press")
+    //      e.preventDefault();
+    //      addItem();
+    //  }
+    
 });
-btnAdd.addEventListener('click',function(){
+btnAdd.addEventListener("click",function(){
+  
     var newListElement = document.createElement('li');
     if(currentInputValue !== ""){
+    
     var textNode = document.createTextNode(currentInputValue);  
     newListElement.appendChild(textNode);
     list.appendChild(newListElement);
@@ -17,8 +30,8 @@ btnAdd.addEventListener('click',function(){
     else{
         alert("Empty!!");
     }
+    return false;
     
-    
-    
+
 
 });
